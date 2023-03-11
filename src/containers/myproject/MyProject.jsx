7507d0e7832737370af41a1c312c05f205ff41af
Code-Alexandre <83603009+Code-Alexandre@users.noreply.github.com>
2023-03-11 00:57:10 +0000

    MyProject and Feature update

--- src/containers/myproject/MyProject.jsx
+++ src/containers/myproject/MyProject.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './myproject.css';
+/* import { Feature } from '../../components'; */
+import Feature from '../../components/feature/Feature';
 
 const MyProject = () => {
   return (
-    <div>
-      MyProject
+    <div className="code__myprojects section__margin" id="myprojects">
+      <div className="code__myprojects-feature">
+      <Feature title="Full stack" text="I am passionate about information technology, and I would love to have the opportunity to take on new challenges" />
+      </div>
+      <div className="code__myprojects-heading">
+        <h1 className="gradient__text">Project i did during my career</h1>
+        <p>Explore my project</p>
+      </div>
+      <div className="code__myprojects-container">
+      <Feature title="Rocket Elevators" text="It's a website made to sell elevator, get a quote it as been made using Ruby on Rails, RestAPI, Nodejs." />
+      <Feature title="Bloc Direct" text="Bloc Direct is a web site made to sell building, buy building and get a evaluation it as been made using Ruby on Rails." />
+      <Feature title="Education" text="I did a boot camp with Codeboxx four month i have learn HTML, CSS/SASS, JavaScript, Node Express, React, Python, Panda, Ruby on Rails, Microsoft.Net, C Sharp, SQL" />
+      </div>
     </div>
   )
 }
